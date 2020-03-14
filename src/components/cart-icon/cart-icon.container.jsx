5eb37b8104cd05e2c67e33b0f23a81eba33cde8d
darrylgit <darrylsmithup@gmail.com>
2020-03-13 22:02:53 -0400

    Refactor cartIcon container with graphql calls

--- src/components/cart-icon/cart-icon.container.jsx
+++ src/components/cart-icon/cart-icon.container.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Mutation, Query } from "react-apollo";
+import { graphql } from "react-apollo";
 import { gql } from "apollo-boost";
+import { flowRight } from "lodash";
 
 import CartIcon from "./cart-icon.component";
 
@@ -16,16 +17,11 @@ const GET_ITEM_COUNT = gql`
   }
 `;
 
-const CartIconContainer = () => (
-  <Query query={GET_ITEM_COUNT}>
-    {({ data: { itemCount } }) => (
-      <Mutation mutation={TOGGLE_CART_HIDDEN}>
-        {toggleCartHidden => (
-          <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
-        )}
-      </Mutation>
-    )}
-  </Query>
+const CartIconContainer = ({ data: { itemCount } }, toggleCartHidden) => (
+  <CartIcon toggleCartHidden={toggleCartHidden} itemCount={itemCount} />
 );
 
-export default CartIconContainer;
+export default flowRight(
+  graphql(GET_ITEM_COUNT),
+  graphql(TOGGLE_CART_HIDDEN, { name: "toggleCartHidden" })
+)(CartIconContainer);
